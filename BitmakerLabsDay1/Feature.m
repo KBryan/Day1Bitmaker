c39c543dc0f59bf080dacc73754a8a736c230b0f
KBryan <kwame@3einteractive.com>
2015-04-06 15:52:03 -0400

    Added Feature Model

---
+++ BitmakerLabsDay1/Feature.m
@@ -0,0 +1,13 @@
+//
+//  Feature.m
+//  BitmakerLabsDay1
+//
+//  Created by Kwame Bryan on 2015-04-06.
+//  Copyright (c) 2015 3e Interactive. All rights reserved.
+//
+
+#import "Feature.h"
+
+@implementation Feature
+
+@end
