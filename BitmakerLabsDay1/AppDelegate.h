66309829f19a105b9e3b656164cdcbf790c378eb
KBryan <kwame@3einteractive.com>
2015-04-06 15:07:06 -0400

    Initial Commit

---
+++ BitmakerLabsDay1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BitmakerLabsDay1
+//
+//  Created by Kwame Bryan on 2015-04-06.
+//  Copyright (c) 2015 3e Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BitmakerLabsDay1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BitmakerLabsDay1
+//
+//  Created by Kwame Bryan on 2015-04-06.
+//  Copyright (c) 2015 3e Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
